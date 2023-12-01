eec638d94c61efcfb2c59676d1a720ad163dcd88
aitouka21 <wscheungae@gmail.com>
2023-12-02 03:41:28 +0800

    formatting

--- day1/main.hs
+++ day1/main.hs
@@ -1,27 +1,27 @@
-import           Control.Monad (ap, liftM2)
-import           Data.Char     (isDigit)
+import Control.Monad (ap, liftM2)
+import Data.Char (isDigit)
 
-p1 :: IO ()
-p1 = solver (filter isDigit)
+main :: IO ()
+main = solver p1 >> solver p2
 
-p2 :: IO ()
-p2 = solver f
-  where
-    f [] = []
-    f s@(x : xs)
-      | x `elem` ['1' .. '9'] = x : f xs
-      | otherwise = case take 5 s of
-         'o' : 'n' : 'e' : _             -> '1' : f xs
-         't' : 'w' : 'o' : _             -> '2' : f xs
-         't' : 'h' : 'r' : 'e' : 'e' : _ -> '3' : f xs
-         'f' : 'o' : 'u' : 'r' : _       -> '4' : f xs
-         'f' : 'i' : 'v' : 'e' : _       -> '5' : f xs
-         's' : 'i' : 'x' : _             -> '6' : f xs
-         's' : 'e' : 'v' : 'e' : 'n' : _ -> '7' : f xs
-         'e' : 'i' : 'g' : 'h' : 't' : _ -> '8' : f xs
-         'n' : 'i' : 'n' : 'e' : _       -> '9' : f xs
-         _                               -> f xs
+p1 :: String -> String
+p1 = filter isDigit
+
+p2 :: String -> String
+p2 [] = []
+p2 s@(x : xs)
+  | x `elem` ['1' .. '9'] = x : p2 xs
+  | otherwise = case take 5 s of
+      'o' : 'n' : 'e' : _ -> '1' : p2 xs
+      't' : 'w' : 'o' : _ -> '2' : p2 xs
+      't' : 'h' : 'r' : 'e' : 'e' : _ -> '3' : p2 xs
+      'f' : 'o' : 'u' : 'r' : _ -> '4' : p2 xs
+      'f' : 'i' : 'v' : 'e' : _ -> '5' : p2 xs
+      's' : 'i' : 'x' : _ -> '6' : p2 xs
+      's' : 'e' : 'v' : 'e' : 'n' : _ -> '7' : p2 xs
+      'e' : 'i' : 'g' : 'h' : 't' : _ -> '8' : p2 xs
+      'n' : 'i' : 'n' : 'e' : _ -> '9' : p2 xs
+      _ -> p2 xs
 
 solver :: (String -> String) -> IO ()
 solver parser = readFile "input.txt" >>= print . sum . map (read . ap [head, last] . pure . parser) . lines
-
