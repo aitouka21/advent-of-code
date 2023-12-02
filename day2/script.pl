2f7b8f57b89f1c7a60c7db844fb1ea2f0367a28e
aitouka21 <wscheungae@gmail.com>
2023-12-03 02:59:47 +0800

    formatting

--- day2/script.pl
+++ day2/script.pl
@@ -4,7 +4,6 @@ while(<>) {
     %rgb= ();
     ($game_id) = /(\d+)/;    
     /(\d+) (\w)/ and $rgb{$2} < $1 and $rgb{$2} = $1 for split /[,;]/;
-
     $part1 += $game_id if $rgb{r} < 13 and $rgb{g} < 14 and $rgb{b} < 15;
     $part2 += $rgb{r} * $rgb{g} * $rgb{b};
 }
