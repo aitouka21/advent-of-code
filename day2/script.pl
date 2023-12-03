04a715420b8aca0d339071b33f89153298fa54c2
aitouka21 <wscheungae@gmail.com>
2023-12-03 11:21:01 +0800

    get game id only when necessary

--- day2/script.pl
+++ day2/script.pl
@@ -2,9 +2,8 @@
 
 while(<>) {
     %rgb= ();
-    ($game_id) = /(\d+)/;    
     /(\d+) (\w)/ and $rgb{$2} < $1 and $rgb{$2} = $1 for split /[,;]/;
-    $part1 += $game_id if $rgb{r} < 13 and $rgb{g} < 14 and $rgb{b} < 15;
+    $part1 += (/(\d+)/)[0] if $rgb{r} < 13 and $rgb{g} < 14 and $rgb{b} < 15;
     $part2 += $rgb{r} * $rgb{g} * $rgb{b};
 }
 print "($part1, $part2)\n";
