b1be0ccbf4f791bb134ce77e26f89cb033fa1f2e
aitouka21 <wscheungae@gmail.com>
2023-12-10 18:19:01 +0800

    chore

--- day10/index.mjs
+++ day10/index.mjs
@@ -1,20 +1,21 @@
+#!/usr/bin/env node
+
 import fs from "fs";
 
+// TODO: rewrite in Haskell / Perl
+// using js to solve is kind of cheating
 const m = fs
   .readFileSync("input.txt", "utf-8")
   .split("\n")
   .map((line) => line.split("").map((x) => [x, false]));
 
-function getStart(m) {
-  for (let i = 0; i < m.length; i++) {
-    for (let j = 0; j < m[i].length; j++) {
+let pos = (() => {
+  for (let i = 0; i < m.length; i++)
+    for (let j = 0; j < m[i].length; j++)
       if (m[i][j][0] === "S") return [i, j];
-    }
-  }
-}
+})();
 
-let pos = getStart(m);
-let count = 0;
+let steps = 0;
 
 do {
   const [x, y] = pos;
@@ -27,19 +28,22 @@ do {
   if (tile === "L") pos = m[x - 1][y][1] ? [x, y + 1] : [x - 1, y];
   if (tile === "J") pos = m[x - 1][y][1] ? [x, y - 1] : [x - 1, y];
   if (tile === "F") pos = m[x][y + 1][1] ? [x + 1, y] : [x, y + 1];
-  count++;
-} while (m[pos[0]][pos[1]][1] !== true);
+  steps++
+} while (!m[pos[0]][pos[1]][1]);
 
-let insideCount = 0;
+let tiles = 0;
+
+// using point in polygon to solve,
+// maybe try to solve it using flood fill / Shoelace formula?
 for (let i = 0; i < m.length; i++) {
-  let hitCount = 0;
+  let crossing = 0;
   for (let j = 0; j < m[i].length; j++) {
     if (m[i][j][1]) {
-      if ("|LJS".includes(m[i][j][0])) hitCount++;
+      if ("|LJS".includes(m[i][j][0])) crossing++;
     } else {
-      if (hitCount % 2 === 1) insideCount++;
+      if (crossing % 2 === 1) tiles++;
     }
   }
 }
 
-console.log(count / 2, insideCount);
+console.log(steps / 2, tiles);
