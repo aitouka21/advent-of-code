cc87528cce4f0fdf739022ed082a7ff3995707b9
aitouka21 <wscheungae@gmail.com>
2023-12-11 23:25:20 +0800

    fix typo

--- day11/index.mjs
+++ day11/index.mjs
@@ -2,42 +2,44 @@
 
 import fs from "fs";
 
-const data = fs
-  .readFileSync("./input.txt", "utf8")
+const m = fs
+  .readFileSync("input.txt", "utf8")
   .split("\n")
-  .filter((x) => x)
+  .slice(0, -1)
   .map((x) => x.split(""));
 
-let mx = data.map((x) => x.every((x) => x === "."));
-let my = data[0].map((_, i) => data.map((x) => x[i]).every((x) => x === "."));
+const scaleX = m.map((x) => x.every((x) => x === "."));
+const scaleY = m[0].map((_, i) => m.map((x) => x[i]).every((x) => x === "."));
 
 let galaxies = [];
 
-for (let i = 0; i < data.length; i++) {
-  for (let j = 0; j < data[0].length; j++) {
-    if (data[i][j] === "#") {
+for (let i = 0; i < m.length; i++) {
+  for (let j = 0; j < m[0].length; j++) {
+    if (m[i][j] === "#") {
       galaxies.push([i, j]);
     }
   }
 }
 
-function solve(multipler) {
+function solve(scale) {
   let sum = 0;
   for (let i = 0; i < galaxies.length; i++) {
     for (let j = i + 1; j < galaxies.length; j++) {
       const [x1, y1] = galaxies[i];
       const [x2, y2] = galaxies[j];
+
       const [xstart, xend] = x1 < x2 ? [x1, x2] : [x2, x1];
       let dx = 0;
       for (let x = xstart + 1; x <= xend; x++) {
-        dx += mx[x] ? multipler : 1;
+        dx += scaleX[x] ? scale : 1;
       }
 
       const [ystart, yend] = y1 < y2 ? [y1, y2] : [y2, y1];
       let dy = 0;
       for (let y = ystart + 1; y <= yend; y++) {
-        dy += my[y] ? multipler : 1;
+        dy += scaleY[y] ? scale : 1;
       }
+
       sum += dx + dy;
     }
   }
