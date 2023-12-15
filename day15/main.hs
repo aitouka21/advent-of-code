717b6289f8e2eda4d6af7769f3396658bbc4e38b
Wilson Cheung <wilson.cheung@offbeattech.com>
2023-12-15 19:52:02 +0800

    chore

--- day15/main.hs
+++ day15/main.hs
@@ -20,9 +20,6 @@ command s = case splitOn '=' s of
   [s, n] -> Set s (read n)
   [s]    -> Remove (init s)
 
-m ::  M.Map Int [(String, Int)]
-m = M.fromList $ map (, []) [0..255]
-
 hash :: String -> Int
 hash = foldl' f 0
   where f acc c = (acc + ord c) * 17 `mod` 256
@@ -31,18 +28,19 @@ op :: Command -> [(String, Int)] -> [(String, Int)]
 op (Remove s) xs      = filter ((/= s) . fst) xs
 op (Set s n) []       = [(s, n)]
 op c@(Set s n) (x:xs) | fst x == s = (s, n) : xs
-                     | otherwise  = x : op c xs
+                      | otherwise  = x : op c xs
 
-adjust :: Command -> M.Map Int [(String, Int)] -> M.Map Int [(String, Int)]
-adjust command = M.adjust (op command) (hash (label command))
+exec :: [Command] -> M.Map Int [(String, Int)]
+exec = foldl' adjust (M.fromList $ map (, []) [0..255])
+  where adjust m c = M.adjust (op c) (hash (label c)) m
 
 sum' :: M.Map Int [(String, Int)] -> Int
 sum' = M.foldlWithKey' g 0
-  where g acc hash xs = sum (zipWith (\pos length -> (hash + 1) * pos * length) [1..] (map snd xs)) + acc
+  where g acc hash xs = sum (zipWith (\pos l -> (hash + 1) * pos * l) [1..] (map snd xs)) + acc
 
 main :: IO ()
 main = do
   [inputs] <-  map (splitOn ',') . lines <$> getContents
   let p1 = sum $ map hash inputs
-  let p2 = sum' $ foldl' (flip adjust) m $ map command inputs
+  let p2 = sum' $ exec $ map command inputs
   print (p1, p2)
